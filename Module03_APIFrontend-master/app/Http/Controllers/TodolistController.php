9c7fc6bea9c2473a16c51db65af640c7c06f1aac
isaiashub <57382403+isaiashub@users.noreply.github.com>
2021-08-28 03:38:16 +0800

    Add files via upload

---
+++ Module03_APIFrontend-master/app/Http/Controllers/TodolistController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $todolist = Todolist::latest()->get();
+        return view('Welcome')->with('todolist',$todolist);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validate request
+        $validatedData =$request->validate([
+            'title'=>'required|max::255',
+        ]);
+        $todolist = Todolist::create([
+            'title'=> $request->title,
+            'completed'=> 0,
+        ]);
+            //return to the homepage
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todolist $todolist)
+    {
+        //
+        $todolist->update($request->all());
+        
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todolist $todolist)
+    {
+        //
+        // $todolist->delete();
+        // return redirect('/');
+    }
+}
